f19ffdadf19f4e661f25bffdc0c0968864ca485a
Lukas Altun <laltun@student.ethz.ch>
2024-07-23 21:05:42 +0200

    job creation trying out

--- benchmarking/job_creation_benchmarking.py
+++ benchmarking/job_creation_benchmarking.py
@@ -1,7 +1,7 @@
 def dict_creation(
     opt: str,
-    dataset: str = "CIFAR100_cutout",
-    model: str = "wideresnet28x10",
+    dataset: str = "CIFAR10_cutout",
+    model: str = "resnet18",
     rho: float = 0.2,
     t: float = 0.4,
     w: float = 1e-3,
@@ -10,8 +10,8 @@ def dict_creation(
     crt_p: float = 0.5,
     crt_z: float = 1.0,
     crt_s: str = "[100-200]",
-    epochs: int = 200,
-    dataset_nn_combination: str = "cifar100_wrn28-10",
+    epochs: int = 5,
+    dataset_nn_combination: str = "cifar10_rn18",
 ):
     d = {}
     d["dataset"] = dataset
@@ -31,7 +31,7 @@ def dict_creation(
 
 
 baseline_opts = ["sgd", "sam", "vasso", "adamw"]
-crt_opts = ["vassore-sgd", "vassoremu-sgd"]
+crt_opts = ["vassore", "vassoremu"]
 crts = ["naive", "random", "schedule"]
 ks = [2, 5, 10, 20, 100]
 ps = [0.5, 0.2, 0.1, 0.05, 0.01]
@@ -39,12 +39,15 @@ ss_endblock = ["[100-200]", "[134-200]", "[160-200]", "[180-200]", "[198-200]"]
 
 
 # Filling of experiment creation commands
-def filling_out_experiment_commands():
+def filling_out_experiment_commands() -> list:
     experiments = []
 
     for baseline_opt in baseline_opts:
+        if baseline_opt == "sgd":
+            experiments.append(dict_creation(baseline_opt, w=5e-4))
+            continue
         experiments.append(dict_creation(baseline_opt))
-        experiments.append(dict_creation("vasso", t=0.2))
+    experiments.append(dict_creation("vasso", t=0.2))
 
     for crt_opt in crt_opts:
         for crt in crts:
@@ -57,6 +60,8 @@ def filling_out_experiment_commands():
             if crt == "schedule":
                 for s in ss_endblock:
                     experiments.append(dict_creation(crt_opt, crt=crt, crt_s=s))
+
+    return experiments
 
 
 command = """
@@ -82,3 +87,5 @@ def benchmarking_experiments():
     for experiment in filling_out_experiment_commands():
         command_content = command.format(**experiment)
         script_commands.append(command_content)
+
+    return script_commands
