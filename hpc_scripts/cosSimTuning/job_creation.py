6343028bda617bb57bd2b13824eb753fb8b14829
Lukas Altun <laltun@student.ethz.ch>
2024-08-05 18:01:42 +0200

    CIFAR100 cosSim Experiments proper

---
+++ hpc_scripts/cosSimTuning/job_creation.py
@@ -0,0 +1,89 @@
+import os
+
+
+def dict_creation(
+    name: str,
+    opt: str,
+    seed: int,
+    dir: str = "cosSim",
+    mem: str = "2G",
+    dataset: str = "CIFAR100_cutout",
+    model: str = "wideresnet28x10",
+    rho: float = 0.2,
+    t: float = 0.4,
+    w: float = 1e-3,
+    crt: str = "cosSim",
+    c: float = 0,
+    dataset_nn_combination: str = "cifar100_wrn2810_cosSim",
+):
+    d = {}
+    d["name"] = name + "_" + str(seed)
+    d["output_dir"] = dir
+    d["memcpu"] = mem
+    d["dataset"] = dataset
+    d["model"] = model
+    d["opt"] = opt
+    d["rho"] = rho
+    d["theta"] = t
+    d["weight_decay"] = w
+    d["crt"] = crt
+    d["crt_c"] = c
+    d["seed"] = seed
+    d["dataset_nn_combination"] = dataset_nn_combination
+    return d
+
+
+seeds = [3107, 1234, 42, 87283, 913248]
+crt_opts = ["vassore-sgd", "vassoremu-sgd"]
+cs = [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
+
+
+def cosSim_experiments() -> list:
+    experiments = []
+    for crt_opt in crt_opts:
+        for c in cs:
+            for seed in seeds:
+                name = f"cosSim={c}"
+                experiments.append(
+                    dict_creation(name=name, opt=crt_opt, seed=seed, c=c)
+                )
+    return experiments
+
+
+slurm_template = """#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus=1
+#SBATCH --time=22:00:00
+#SBATCH --job-name={name}_c100_wrn2810
+#SBATCH --mem-per-cpu={memcpu}
+#SBATCH --output={output_dir}/outputs/{name}.out
+#SBATCH --error={output_dir}/errors/{name}.err
+#SBATCH --open-mode=truncate
+
+module load eth_proxy
+module load stack/2024-06
+module load python_cuda/3.11.6
+module load py-distro/1.8.0-4tnktx7
+
+cd ~/sam/VaSSO
+
+python3 train.py \
+    --dataset {dataset} \
+    --model {model} \
+    --opt {opt} \
+    --rho {rho} \
+    --theta {theta} \
+    --crt {crt} \
+    --crt_c {c} \
+    --seed {seed} \
+    --dataset_nn_combination {dataset_nn_combination} \
+"""
+
+for experiment in cosSim_experiments():
+    script_content = slurm_template.format(**experiment)
+    output_dir = experiment["output_dir"]
+    os.makedirs(os.path.join(output_dir, "scripts"), exist_ok=True)
+    script_filename = os.path.join(output_dir, "scripts", f"{experiment['name']}.sh")
+    with open(script_filename, "w") as file:
+        file.write(script_content)
