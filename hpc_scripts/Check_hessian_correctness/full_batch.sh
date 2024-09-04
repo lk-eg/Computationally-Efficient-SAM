86de0417aa23fc2b428ac0a06e6cf0ca30bf4de7
Lukas Altun <laltun@student.ethz.ch>
2024-09-04 12:01:58 +0200

    check Hessian calculation for correctness

---
+++ hpc_scripts/Check_hessian_correctness/full_batch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+
+module load eth_proxy
+module load gcc/8.2.0
+module load python_gpu/3.8.5
+
+python3 ../../train.py \
+        --opt sam-sgd \
+        --weight_decay 1e-3 \
+        --rho 0.1 \
+        --model resnet18 \
+        --dataset CIFAR10_cutout \
+        --datadir /cluster/home/laltun/datasets \
+        --epochs 1 \
+        --dataset_nn_combination "cifar10_rn18_hessian_full"
